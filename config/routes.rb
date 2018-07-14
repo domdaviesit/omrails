015783ed89ef1cf43fc734348d3bf345d977b5a4
domdaviesit <domdaviesit@gmail.com>
2018-07-14 20:30:16 +0100

    created about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  # get 'pages/home'
+  # get 'pages/home'  out of the box
+  # website is just a collection of pages that do things
 
   root 'pages#home'
 
+  # about page
+  get 'about' => 'pages#about'
+
+  # contact page
+  get 'contact' => 'pages#contact'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
